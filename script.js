b7a90e496fe68650572e0b2c168e7c56ccb0196e
AdRosing22 <adamrosinghd@gmail.com>
2024-09-23 13:48:26 +0200

    uppdaterad validering

--- script.js
+++ script.js
@@ -1,79 +1,80 @@
 const btn = document.getElementById('btn-send-form')
 
-let isValid = true
-
-const visaKnapp = () => {
-    if (isValid) {
-        btn.style.display = "block"
-    } else {
-        btn.style.display = "none" 
-    }
-}
+btn.disabled = true
 
 const fullName = document.getElementById('full-name')
+const email = document.getElementById('epost')
+const telefon = document.getElementById('tlf')
+const meddelande = document.getElementById('msg')
+
+let nameValid = false
+let epostValid = false
+let telValid = false
+let msgValid = false
+
+const kontrollBtn = () =>{
+    if(nameValid && epostValid && telValid && msgValid){
+        btn.disabled = false
+    }else{
+        btn.disabled = true
+    }
+}
 
 if (fullName) {
     fullName.addEventListener('input', () => {
         const onlyAplhabet = /^[a-zA-Z\s]+$/
         if (fullName.value.length <= 2 || !onlyAplhabet.test(fullName.value)) {
-            fullName.classList.add('error')
-            isValid = false
-             
+            fullName.classList.add('error') 
+            nameValid = false   
         } else {
             fullName.classList.remove('error')
-            isValid = true
+            nameValid = true
         } 
-        visaKnapp()
+        kontrollBtn()
     })
 }
 
-const email = document.getElementById('epost')
     
 if (email) {
     email.addEventListener('input', () => {
         const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
         if (!emailValidation.test(email.value)) {
             email.classList.add('error')
-            isValid = false
+            epostValid = false   
         } else {
             email.classList.remove('error')
-            isValid = true
+            epostValid = true
         }
-        visaKnapp()
+        kontrollBtn()
     })
 }
-
-const telefon = document.getElementById('tlf')
 
 if (telefon) {
     telefon.addEventListener('input', () => {
         const phonePattern = /^[+\d-]*\d+[+\d-]*$/
         const onlyDigitsAndSymbols = /^[+\d-]+$/
-    
         if (telefon.value.trim().length < 9 || telefon.value.trim().length > 15 || !onlyDigitsAndSymbols.test(telefon.value.trim()) 
             || !phonePattern.test(telefon.value.trim())) {
             telefon.classList.add('error') 
-            isValid = false            
+            telValid = false            
         } else {
             telefon.classList.remove('error')
-            isValid = true
+            telValid = true
         }
-        visaKnapp()
+        kontrollBtn()
     })
 }
 
-const meddelande = document.getElementById('msg')
-
 if(meddelande){
     meddelande.addEventListener('input', () => {
-        if(meddelande.value.trim().length === 201){
-            meddelande.classList.add('error') 
-            isValid = false          
+        if(meddelande.value.trim().length === 201 || meddelande.value.trim().length === 0){
+            meddelande.classList.add('error')         
+            msgValid = false   
         }else{
             meddelande.classList.remove('error')
-            isValid = true
+            msgValid = true
         }
-        visaKnapp()
+        kontrollBtn()
     })
 }
 
