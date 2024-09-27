956c93af0774f013ff19e41e919a896205fa88a7
AdRosing22 <adamrosinghd@gmail.com>
2024-09-27 08:22:49 +0200

    uppdaterat script

--- script.js
+++ script.js
@@ -21,63 +21,62 @@ const kontrollBtn = () =>{
     }
 }
 
-if (fullName) {
-    fullName.addEventListener('input', () => {
-        const onlyAplhabet = /^[a-zA-Z\s]+$/
-        if (fullName.value.length <= 2 || !onlyAplhabet.test(fullName.value)) {
-            fullName.classList.add('error') 
-            nameValid = false   
-        } else {
-            fullName.classList.remove('error')
-            nameValid = true
-        } 
-        kontrollBtn()
-    })
-}
+
+fullName.addEventListener('input', () => {
+    const onlyAplhabet = /^[a-zA-Z\s]+$/
+    if (fullName.value.length <= 2 || !onlyAplhabet.test(fullName.value)) {
+        fullName.classList.add('error') 
+        nameValid = false   
+    } else {
+        fullName.classList.remove('error')
+        nameValid = true
+    } 
+    kontrollBtn()
+})
+
 
     
-if (email) {
-    email.addEventListener('input', () => {
-        const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        if (!emailValidation.test(email.value)) {
-            email.classList.add('error')
-            epostValid = false   
-        } else {
-            email.classList.remove('error')
-            epostValid = true
-        }
-        kontrollBtn()
-    })
-}
+email.addEventListener('input', () => {
+    const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailValidation.test(email.value)) {
+        email.classList.add('error')
+        epostValid = false   
+    } else {
+        email.classList.remove('error')
+        epostValid = true
+    }
+    kontrollBtn()
+})
 
-if (telefon) {
-    telefon.addEventListener('input', () => {
-        const phonePattern = /^[+\d-]*\d+[+\d-]*$/
-        const onlyDigitsAndSymbols = /^[+\d-]+$/
-        if (telefon.value.trim().length < 9 || telefon.value.trim().length > 15 || !onlyDigitsAndSymbols.test(telefon.value.trim()) 
-            || !phonePattern.test(telefon.value.trim())) {
-            telefon.classList.add('error') 
-            telValid = false            
-        } else {
-            telefon.classList.remove('error')
-            telValid = true
-        }
-        kontrollBtn()
-    })
-}
 
-if(meddelande){
-    meddelande.addEventListener('input', () => {
-        if(meddelande.value.trim().length === 201 || meddelande.value.trim().length === 0){
-            meddelande.classList.add('error')         
-            msgValid = false   
-        }else{
-            meddelande.classList.remove('error')
-            msgValid = true
-        }
-        kontrollBtn()
-    })
-}
+
+telefon.addEventListener('input', () => {
+    const phonePattern = /^[+\d-]*\d+[+\d-]*$/
+    const onlyDigitsAndSymbols = /^[+\d-]+$/
+    if (telefon.value.trim().length < 9 || telefon.value.trim().length > 15 || !onlyDigitsAndSymbols.test(telefon.value.trim()) 
+    || !phonePattern.test(telefon.value.trim())) {
+        telefon.classList.add('error') 
+        telValid = false            
+    } else {
+        telefon.classList.remove('error')
+        telValid = true
+    }
+    kontrollBtn()
+})
+
+
+
+meddelande.addEventListener('input', () => {
+    if(meddelande.value.trim().length === 201 || meddelande.value.trim().length === 0){
+        meddelande.classList.add('error')         
+        msgValid = false   
+    }else{
+        meddelande.classList.remove('error')
+        msgValid = true
+    }
+    kontrollBtn()
+})
+
 
 
 //hamburger
