b593058be97ad99304c4c17cde3cf2cd6c59ac54
AdRosing22 <adamrosinghd@gmail.com>
2024-10-04 10:40:17 +0200

    kommentarer på adams kod

--- Adam/JavaScript/animation.js
+++ Adam/JavaScript/animation.js
@@ -1,17 +1,18 @@
+//skapar en ny IntersectionObserver
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
+        if(entry.isIntersecting){ //ifall ett element är i viewporten
+            entry.target.classList.add('show') //lägg till klassen som får det att komma in från vänster
         }else{
             entry.target.classList.remove('show')
         }
     })
 })
 
-const elements = document.querySelectorAll('.scroll')
-elements.forEach((el) => observer.observe(el))
+const elements = document.querySelectorAll('.scroll') 
+elements.forEach((el) => observer.observe(el)) //observar alla element med klassen scroll, vilket är varje grid element 1-4
 
-
+//skapar en till intersectionobserver för att animera skillbarsen varje gång de är synliga i viewporten
 const skillsObserver = new IntersectionObserver((entries) =>{
     entries.forEach((entry) => {
         if(entry.isIntersecting){
@@ -21,7 +22,7 @@ const skillsObserver = new IntersectionObserver((entries) =>{
         }
     })
 }, {
-    threshold: 0.5
+    threshold: 0.5 //börja köra när 50% av elementet är i viewporten
 })
 
 const skillBars = document.querySelectorAll('.kompetens')
